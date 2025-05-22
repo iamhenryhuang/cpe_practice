efccdaa6f8fdc31906d83eb3ac11c43a7820d7c1
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2025-05-22 14:33:14 +0800

    Update pE.cpp

--- 0520/pE.cpp
+++ 0520/pE.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> coins = {1, 5, 10}; // 硬幣面值
+    vector<long long> dp(30001, 0); // dp[x] 表示湊出 x cent的方法數
+    dp[0] = 1; // 初始化，湊出 0 cent的方法只有 1 種
+
+    // dp
+    for (int coin : coins) {
+        for (int i = coin; i <= 30000; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+
+    int total;
+    while (cin >> total, total != 0) {
+        if (dp[total] == 1) {
+            cout << "There is only 1 way to produce " << total << " cents change.\n";
+        } else {
+            cout << "There are " << dp[total] << " ways to produce " << total << " cents change.\n";
+        }
+    }
+
+    return 0;
+}
+
+/*#include <iostream>
 using namespace std;
 
 int dime = 10;
@@ -7,7 +35,7 @@ int penny = 1;
 
 int main() {
     int total;
-    while(cin >> total, total != 0) {
+    while(cin >> total, total != 0, total <= 30000) {
         int ways = 0;
         // i, j, k 分別代表了使用了多少個 dime, nickel 和 penny
         for(int i = 0; i <= total / dime; i++) {
@@ -28,4 +56,4 @@ int main() {
     }
 
     return 0;
-}
+}*/
